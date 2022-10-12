99d52bc0d01e44ccc7531f42027fc9add8fcc9e3
Arko <arkoroyb@gmail.com>
2022-10-12 17:26:59 +0600

    blogs ans uploaded

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -12,7 +12,7 @@ const Blog = () => {
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            
+                            ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route
                         </div>
                     </div>
                 </div>
@@ -24,7 +24,7 @@ const Blog = () => {
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            
+                            The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux
                         </div>
                     </div>
                 </div>
@@ -36,7 +36,8 @@ const Blog = () => {
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            
+                            A hook is a special function which enables one use state and other React features without writing ES6 class components which are generally considered difficult to understand, use and master.
+                            useRef hook is part of the React Hooks API. It is a function which takes a maximum of one argument and returns an Object. The returned object has a property called current whose value is the argument passed to useRef. If we invoke it without an argument, the returned object's current property is set to undefined.
                         </div>
                     </div>
                 </div>
