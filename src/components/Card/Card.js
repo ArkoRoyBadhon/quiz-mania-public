42c54621af40cf5c61e8b4759219f37bb7a01333
Arko <arkoroyb@gmail.com>
2022-10-12 18:53:51 +0600

    responsiveness fixed and banner-area upload

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ import './Card.css'
 const Card = ({ topic, handleSelect }) => {
     const { id, name, logo } = topic;
     return (
-        <div className='col-12 col-md-4'>
+        <div className='col-12 col-md-6 col-lg-4'>
             <div className="border card-info rounded-3">
                 <div className="image p-2">
                     <img className='img-fluid' src={logo} alt="" />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import Card from '../Card/Card';
+import './Home.css'
 
 const Home = () => {
     const topics = useLoaderData();
     const navigate = useNavigate();
-    // console.log(topics);
     const handleSelect = (id) => {
-        // console.log(id);
         navigate(`/quiz/${id}`)
     }
+
     return (
-        <div className='container'>
-            <div className="row g-3 mt-3">
-                {
-                    topics.data.map(topic =>
-                        <Card key={topic.id} topic={topic} handleSelect={handleSelect}></Card>
-                    )
-                }
+        <div className="">
+            <div className="home-bg">
+                <div id="carouselExampleSlidesOnly" class="carousel" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div className="img">
+                                <img className='img' src={process.env.PUBLIC_URL + "/image/quiz.jpg"} class="d-block w-100" alt="..." />
+                            </div>
+                            
+                            <div class="carousel-caption d-md-block  div-info">
+                                <div >
+                                    <h1>Welcome to Our Quiz Mania</h1>
+                                    <p>You can test youself by giving our quiz</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className="row g-3 mt-3">
+                    {
+                        topics.data.map(topic =>
+                            <Card key={topic.id} topic={topic} handleSelect={handleSelect}></Card>
+                        )
+                    }
+                </div>
             </div>
         </div>
     );
